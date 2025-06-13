ea582172889a803a9a9971ee89461824dfeab344
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-06-13 18:48:08 -0300

    [COGF-20] Removendo component ProductDetails (#19)
    ## [COGF-20] Descrição
    Eu ia acrescentar uma seção extra além da requisitada na descrição do projeto, mas decidi me ater apenas ao necessário para evitar desperdício de tempo. Por isso removi a pasta sections

--- src/components/content/ProductContent/ProductContent.jsx
+++ src/components/content/ProductContent/ProductContent.jsx
@@ -1,11 +1,30 @@
-import { Container } from '@/components/common';
-import { ProductDetails } from './sections';
+import { Container, Button, Card, ImagePlaceholder } from '@/components/common';
+import { parseMoney } from '@/helpers/parse';
 
 export default function ProductContent({ product }) {
    return (
       <div className="ProductContent">
          <Container>
-            <ProductDetails product={product} />
+            <Card className="product-details" padding="none" elevation="xl">
+               <ImagePlaceholder />
+
+               <div className="product-info">
+                  <p className="product-path">{product.category} / {product.name}</p>
+                  <h1>{product.name}</h1>
+
+                  <div className="price-group">
+                     <span className="label">Price:</span>
+                     <p className="price">{parseMoney(product.price)}</p>
+                  </div>
+
+                  <Button className="add-to-cart" type="cta" fullwidth>Adicionar ao Carrinho</Button>
+
+                  <div className="description-wrap">
+                     <span className="label">Descrição:</span>
+                     <p className="description">{product.description}</p>
+                  </div>
+               </div>
+            </Card>
          </Container>
       </div>
    );
