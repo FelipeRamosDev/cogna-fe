3b456fed3b968d1f637d4aa3ecc9b0a83f66e6b3
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-06-12 23:55:49 -0300

    [COGF-7] Criar o layout básico de página (#7)
    ## [COGF-7] Descrição
    Eu criei o component PageBase que servirá como template básico para a maioria das páginas. Junto com ele eu criei os seguintes componentes que eram necessários para a implementação:
    - Container
    - HeaderBase

---
+++ src/components/headers/HeaderBase/HeaderBase.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/common";
+
+export default function HeaderBase() {
+   return (
+      <header className="HeaderBase">
+         <Container>
+            <span className="logo">CognaShop</span>
+         </Container>
+      </header>
+   );
+}
