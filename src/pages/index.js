b1628a15e4dc9a8a59d189b2a7f613d7a5e9ae9f
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-06-13 00:14:55 -0300

    [COGF-6] Componentes Base (#9)
    * [COGF-9] Tema do projeto (#6)
    Implementei a folha tema do projeto.
    
    * [COGF-7] Criar o layout básico de página (#7)
    Eu criei o component PageBase que servirá como template básico para a maioria das páginas. Junto com ele eu criei os seguintes componentes que eram necessários para a implementação: `Container` e `HeaderBase`
    
    * [COGF-8] Instância AJAX (#8)
    Criei o serviço AJAX que é uma instância Axios.

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import HomeContent from "@/components/Home";
-
-export default function Home() {
-  return <HomeContent />;
-}
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import { PageBase } from '@/components/layout';
+import HomeContent from '@/components/Home';
+
+export default function HomePage() {
+  return (
+    <PageBase>
+      <HomeContent />
+    </PageBase>
+  );
+}
