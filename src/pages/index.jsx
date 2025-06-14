1c048a7e4415e3c59900ccdc96b05ab13825121d
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-06-13 22:50:56 -0300

    [COGF-26] Metatags nas páginas (#26)
    ## [COGF-26] Descrição
    Implementei as metatags nas páginas utilizando next/head como abordagem.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import { PageBase } from '@/components/layout';
 import { HomeContent } from '@/components/content';
 import AJAX from '@/services/AJAX';
+import Head from 'next/head';
 
 export async function getServerSideProps() {
   try {
@@ -15,8 +16,32 @@ export async function getServerSideProps() {
 }
 
 export default function HomePage({ products }) {
+  const META_TITLE = 'Cogna - Todos produtos';
+  const META_DESCRIPTION = 'Página principal com listagem de todos os produtos da loja.';
+  const META_KEYWORDS = 'produtos, loja, cogna, comprar, ecommerce';
+  const META_AUTHOR = 'Felipe Ramos';
+  const META_IMAGE = 'http://localhost/og-image.jpg';
+  const META_URL = 'http://localhost/';
+
   return (
     <PageBase>
+      <Head>
+        <title>{META_TITLE}</title>
+        <meta name="description" content={META_DESCRIPTION} />
+        <meta name="keywords" content={META_KEYWORDS} />
+        <meta name="author" content={META_AUTHOR} />
+        <meta property="og:title" content={META_TITLE} />
+        <meta property="og:description" content={META_DESCRIPTION} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={META_URL} />
+        <meta property="og:image" content={META_IMAGE} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={META_TITLE} />
+        <meta name="twitter:description" content={META_DESCRIPTION} />
+        <meta name="twitter:image" content={META_IMAGE} />
+        <link rel="canonical" href={META_URL} />
+      </Head>
+
       <HomeContent products={products} />
     </PageBase>
   );
--- src/pages/produto/[id].jsx
+++ src/pages/produto/[id].jsx
@@ -1,6 +1,7 @@
 import AJAX from '@/services/AJAX';
 import { ProductContent } from '@/components/content';
 import { PageBase } from '@/components/layout';
+import Head from 'next/head';
 
 export async function getServerSideProps(context) {
    const productID = context.query.id;
@@ -17,8 +18,32 @@ export async function getServerSideProps(context) {
 }
 
 export default function Product({ product }) {
+   const META_TITLE = `Cogna - ${product.name}`;
+   const META_DESCRIPTION = product.description;
+   const META_KEYWORDS = `produtos, loja, cogna, comprar, ecommerce, ${product.name}`;
+   const META_AUTHOR = 'Felipe Ramos';
+   const META_IMAGE = 'http://localhost/og-image.jpg';
+   const META_URL = 'http://localhost/';
+
    return (
       <PageBase>
+         <Head>
+            <title>{META_TITLE}</title>
+            <meta name="description" content={META_DESCRIPTION} />
+            <meta name="keywords" content={META_KEYWORDS} />
+            <meta name="author" content={META_AUTHOR} />
+            <meta property="og:title" content={META_TITLE} />
+            <meta property="og:description" content={META_DESCRIPTION} />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={META_URL} />
+            <meta property="og:image" content={META_IMAGE} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={META_TITLE} />
+            <meta name="twitter:description" content={META_DESCRIPTION} />
+            <meta name="twitter:image" content={META_IMAGE} />
+            <link rel="canonical" href={META_URL} />
+         </Head>
+
          <ProductContent product={product} />
       </PageBase>
    );
