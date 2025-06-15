b7ab063cc4ae7e882a4af28bc0038c68ca33b70d
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-06-15 14:19:00 -0300

    [COGF-33] Página e component content para cadastrar produto (#30)
    ## [COGF-33] Descrição
    Apenas criando os componentes necessários para acessar a nova rota /produto/novo

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,2 +1,3 @@
 export { default as HomeContent } from './HomeContent/HomeContent';
-export { default as ProductContent } from './ProductContent/ProductContent';
+export { default as ProductContent } from './produto/ProductContent/ProductContent';
+export { default as NewProductContent } from './produto/NewProductContent/NewProductContent';
---
+++ src/components/content/produto/NewProductContent/NewProductContent.jsx
@@ -0,0 +1,10 @@
+import { Container } from '@/components/common';
+
+export default function NewProductContent() {
+   return (
+      <Container className="NewProductContent">
+         <h1>Novo Produto</h1>
+         <p>Cadastre abaixo as informações do novo produto.</p>
+      </Container>
+   );
+}
