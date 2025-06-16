0348d0b552dd9a55e937332456ee9c32ac58ffad
FelipeRamosDev <felipe@feliperamos.info>
2025-06-16 19:01:05 -0300

    [COGF-31] Arrumando URLs

--- src/pages/produto/importar.jsx
+++ src/pages/produto/importar.jsx
@@ -1,10 +1,18 @@
 import { ImportContent } from '@/components/content';
 import { PageBase } from '@/components/layout';
 
-export default function Import() {
+export async function getServerSideProps() {
+   return {
+      props: {
+         CLIENT_API_ROOT: process.env.CLIENT_API_ROOT
+      }
+   };
+}
+
+export default function Import({ CLIENT_API_ROOT }) {
    return (
       <PageBase>
-         <ImportContent />
+         <ImportContent CLIENT_API_ROOT={CLIENT_API_ROOT} />
       </PageBase>
    );
 };
