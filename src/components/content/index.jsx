ec26f707f5831bdb55ba744dd8d911dda84a2064
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-06-16 19:19:36 -0300

    [COGF-31] Importar Produtos (#32)
    * [COGF-32] Atualização dos arquivos Docker (#29)
    Alterando a network e inserindo variavel de ambiente
    
    * [COGF-33] Página e component content para cadastrar produto (#30)
    Apenas criando os componentes necessários para acessar a nova rota /produto/novo
    
    * [COGF-35] Importar produtos para o banco de dados (#31)
    Criei uma página para fazer um upload de um JSON com os produtos para o servidor.

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,2 +1,3 @@
 export { default as HomeContent } from './HomeContent/HomeContent';
-export { default as ProductContent } from './ProductContent/ProductContent';
+export { default as ProductContent } from './produto/ProductContent/ProductContent';
+export { default as ImportContent } from './produto/ImportContent/ImportContent';
--- src/services/AJAX.js
+++ src/services/AJAX.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const AJAX = axios.create({
    baseURL: process.env.API_ROOT || 'http://localhost:8000',
-   timeout: 10000
+   timeout: 30000
 });
 
 export default AJAX;
