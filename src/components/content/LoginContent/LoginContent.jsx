52eac37d28575ed1c51ec81cbdfe0e6e521ee7ae
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-06-18 09:22:31 -0300

    [COGF-38] Criar base para página de Login (#35)
    * [COGF-38] Descrição
    Criei os arquivos necessários para implementar a nova página de login

---
+++ src/components/content/LoginContent/LoginContent.jsx
@@ -0,0 +1,13 @@
+import { Card, Container } from '@/components/common';
+
+export default function LoginContent() {
+   return (
+      <div className="LoginContent">
+         <Container>
+            <Card className="login-card">
+               <h1 className="card-title">Login</h1>
+            </Card>
+         </Container>
+      </div>
+   );
+}
--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,3 +1,6 @@
 export { default as HomeContent } from './HomeContent/HomeContent';
+export { default as LoginContent } from './LoginContent/LoginContent';
+
+// /produto
 export { default as ProductContent } from './produto/ProductContent/ProductContent';
 export { default as ImportContent } from './produto/ImportContent/ImportContent';
