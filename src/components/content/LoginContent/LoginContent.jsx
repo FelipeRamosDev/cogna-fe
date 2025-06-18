fdf4cdc72bfbd373105241cd8fcccca588dd7ede
FelipeRamosDev <felipe@feliperamos.info>
2025-06-18 10:22:35 -0300

    [COGF-39] Componente Input
    ## [COGF-39] Descrição
    Criado o component Input reutilizável

--- src/components/content/LoginContent/LoginContent.jsx
+++ src/components/content/LoginContent/LoginContent.jsx
@@ -1,11 +1,13 @@
 import { Card, Container } from '@/components/common';
+import { Input } from '@/components/inputs';
 
 export default function LoginContent() {
    return (
       <div className="LoginContent">
          <Container>
             <Card className="login-card">
-               <h1 className="card-title">Login</h1>
+               <h1 className="card-title">Login de Usuário</h1>
+               <p>Por favor, insira suas credenciais para continuar.</p>
             </Card>
          </Container>
       </div>
--- src/components/inputs/index.jsx
+++ src/components/inputs/index.jsx
@@ -1,1 +1,2 @@
-export { default as InputText } from './FileInput/FileInput';
+export { default as Input } from './Input/Input';
+export { default as FileInput } from './FileInput/FileInput';
