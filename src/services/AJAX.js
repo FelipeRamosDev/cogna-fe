ccdc7f98417879a50abf242329687f83c082f4da
FelipeRamosDev <felipe@feliperamos.info>
2025-06-19 19:57:51 -0300

    [COGF-28] Arrumando erros do Lint

--- src/services/AJAX.js
+++ src/services/AJAX.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-export default (API_ROOT, options) => axios.create({
-   baseURL: API_ROOT || process.env.API_ROOT || 'http://localhost:8000',
-   withCredentials: true,
-   timeout: 30000,
-   ...options
-});
+export default function AJAX(API_ROOT, options) {
+   return axios.create({
+      baseURL: API_ROOT || process.env.API_ROOT || 'http://localhost:8000',
+      withCredentials: true,
+      timeout: 30000,
+      ...options
+   });
+}
