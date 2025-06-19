a31d49b5aa175ed85b304a84ef525582f9921444
FelipeRamosDev <felipe@feliperamos.info>
2025-06-19 14:27:21 -0300

    Update AJAX.js

--- src/services/AJAX.js
+++ src/services/AJAX.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 export default (API_ROOT, options) => axios.create({
    baseURL: API_ROOT || process.env.API_ROOT || 'http://localhost:8000',
    withCredentials: true,
+   timeout: 30000,
    ...options
 });
